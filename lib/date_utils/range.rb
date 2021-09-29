8e219d5c3debd0c8ac7e7ee65102b69b01b19797
Kenneth Leung <kenneth@leungs.us>
2021-09-29 16:21:16 -0700

    Initial commit.

--- lib/date_utils/range.rb
+++ lib/date_utils/range.rb
@@ -1,18 +1,19 @@
 module DateUtils
-    module Range
-        def date_partitions(every: :month, step: 1)
-        a = []
-        while true
-            begin_date = begin_date.nil? ? self.begin : begin_date.next_month(step)
-            end_date = begin_date.next_month(step).prev_day
-            end_date = end_date > self.end ? self.end : end_date
-            if begin_date > self.end
-            break
-            else
-            a << (begin_date..end_date)
-            end
+  module Range
+    def date_partitions(every: :month, step: 1)
+      a = []
+      while true
+        begin_date = begin_date.nil? ? self.begin : begin_date.next_month(step)
+        end_date = begin_date.next_month(step).prev_day
+        end_date = end_date > self.end ? self.end : end_date
+        if begin_date > self.end
+          break
+        else
+          a << (begin_date..end_date)
         end
-        a
-        end
+      end
+      a
     end
+  end
 end
+  
